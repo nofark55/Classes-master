b868a318044e6ff8e1157bf294e6b412c61e4728
nofark55 <noahafarkas@gmail.com>
2025-09-25 19:41:27 -0400

    Implement overloaded flyinthesky methods
    Added overloaded methods for flying behavior.

--- src/RunMyZoo.java
+++ src/RunMyZoo.java
@@ -3,7 +3,20 @@ public class RunMyZoo {
         System.out.println("Mx. Bradford's ZOOO!!!!!!!!!");
         RunMyZoo r = new RunMyZoo();
     }
-    public RunMyZoo(){
+    
+    void flyinthesky(int x) {
+        for (int i = 0; i < x; i++) {
+            System.out.println("flap flap flap");
+        }
+    }
+
+    void flyinthesky(int x, int y) {
+        for (int i = 0; i < x * y; i++) {
+            System.out.println("flap flap");
+        }
+    }
+
+    public RunMyZoo() {
         Elephant e = new Elephant();
         System.out.println(e.getTuskLength()); //jsut finds for testing, to make sure its working
         e.eat();
@@ -17,5 +30,9 @@ public class RunMyZoo {
 
         emu em = new emu();
         em.sleep();
+        flyinthesky(3);
+        flyinthesky(3, 2);
+
     }
+        
 }
